d9a4a522cb4e7f32b08bfbd1a7be6030feab5226
kettily <36549395+besttrainer@users.noreply.github.com>
2019-01-17 02:33:00 +0100

    update payoff of Perf

--- Win32Pricing/src/PerformanceOption.cpp
+++ Win32Pricing/src/PerformanceOption.cpp
@@ -27,17 +27,36 @@ PerformanceOption::PerformanceOption(double T, int nbTimeSteps, int size, PnlVec
  * @return phi(trajectoire)
  */
 double PerformanceOption::payoff(const PnlMat *path) {
+	
 	double payOff = 1;
+	PnlVect* vecteur = pnl_mat_mult_vect(path, weights_);
+	double a0 = pnl_vect_get(vecteur, 0);
+	double aNext;
 	for (int i = 1; i <= nbTimeSteps_; i++)
 	{
-		double sum1 = 0;
-		double sum2 = 0;
-		for (int d = 0; d < size_; d++)
-		{
-			sum1 += GET(weights_, d) * MGET(path, i, d);
-			sum2 += GET(weights_, d) * MGET(path, i - 1, d);
-		}
-		payOff += fmax(sum1 / sum2 - 1, 0.0);
+		aNext = pnl_vect_get(vecteur, i);
+		payOff += fmax(( aNext/ a0 )-1.0, 0.0);
+		a0 = aNext;
 	}
 	return payOff;
+	pnl_vect_free(&vecteur);
+	
+	/*
+	double payOff = 1;
+	PnlVect* vecteur = pnl_mat_mult_vect(path, weights_);
+
+	PnlVect* vecteur2 = pnl_vect_create_subvect(vecteur, 1, vecteur->size-1);
+	pnl_vect_resize(vecteur2, vecteur->size);
+	pnl_vect_div_vect_term(vecteur2,vecteur);
+
+	for (int i = 1; i <= nbTimeSteps_; i++)
+	{	
+		payOff += fmax(pnl_vect_get(vecteur2, i-1) - 1.0, 0.0);
+	}
+	return payOff;
+	pnl_vect_free(&vecteur);
+	pnl_vect_free(&vecteur2);
+	*/
+
 }
+
